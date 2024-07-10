02da92471322f49e64bcd939467769eb6fc4807e
Darth Affe <darthaffe@wyrez.org>
2024-07-11 00:24:12 +0200

    Added first extensions to work with System.Drawing-bitmaps

---
+++ HPPH.System.Drawing/ImageExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
+namespace HPPH.System.Drawing;
+
+public static class ImageExtension
+{
+    public static Bitmap ToBitmap(this IImage image)
+    {
+        throw new NotImplementedException();
+    }
+
+    [SupportedOSPlatform("windows")]
+    public static unsafe IImage ToImage(this Bitmap bitmap)
+    {
+        BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+        ReadOnlySpan<byte> buffer = new(data.Scan0.ToPointer(), data.Stride * data.Height);
+
+        IImage image;
+
+        if (data.PixelFormat.HasFlag(PixelFormat.Format24bppRgb))
+            image = Image<ColorRGB>.Create(buffer, data.Width, data.Height, data.Stride);
+        else if (data.PixelFormat.HasFlag(PixelFormat.Format32bppArgb))
+            image = Image<ColorARGB>.Create(buffer, data.Width, data.Height, data.Stride);
+        else throw new NotSupportedException($"Unsupported pixel format '{bitmap.PixelFormat}'.");
+
+        bitmap.UnlockBits(data);
+
+        return image;
+    }
+}
---
+++ HPPH.System.Drawing/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Runtime.Versioning;
+
+namespace HPPH.System.Drawing;
+
+public static class ImageHelper
+{
+    [SupportedOSPlatform("windows")]
+    public static IImage LoadImage(string path)
+    {
+        using Bitmap bmp = new(path);
+        return bmp.ToImage();
+    }
+
+    [SupportedOSPlatform("windows")]
+    public static IImage LoadImage(Stream stream)
+    {
+        using Bitmap bmp = new(stream);
+        return bmp.ToImage();
+    }
+}
