5126980e97b5c91286fc14667db43896be988a4f
Darth Affe <darthaffe@wyrez.org>
2024-07-15 00:00:58 +0200

    Added extensions for SkiaSharp

---
+++ HPPH.SkiaSharp/ImageExtension.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using SkiaSharp;
+
+namespace HPPH.SkiaSharp;
+
+public static class ImageExtension
+{
+    public static unsafe SKImage ToSKImage(this IImage image) => SKImage.FromBitmap(image.ToSKBitmap());
+
+    public static unsafe SKBitmap ToSKBitmap(this IImage image)
+    {
+        SKBitmap bitmap = new(image.Width, image.Height, SKColorType.Bgra8888, SKAlphaType.Unpremul);
+        nint pixelPtr = bitmap.GetPixels(out nint length);
+        image.ConvertTo<ColorBGRA>().CopyTo(new Span<byte>((void*)pixelPtr, (int)length));
+
+        return bitmap;
+    }
+
+    public static byte[] ToPng(this IImage image)
+    {
+        using SKImage skImage = image.ToSKImage();
+        return skImage.Encode(SKEncodedImageFormat.Png, 100).ToArray();
+    }
+
+    public static IImage ToImage(this SKImage skImage) => SKBitmap.FromImage(skImage).ToImage();
+
+    public static IImage ToImage(this SKBitmap bitmap)
+        => Image<ColorBGRA>.Create(MemoryMarshal.Cast<SKColor, ColorBGRA>(bitmap.Pixels), bitmap.Width, bitmap.Height);
+}
---
+++ HPPH.SkiaSharp/ImageHelper.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp;
+
+namespace HPPH.SkiaSharp;
+
+public static class ImageHelper
+{
+    public static IImage LoadImage(string path)
+    {
+        using SKImage image = SKImage.FromEncodedData(path);
+        return image.ToImage();
+    }
+
+    public static IImage LoadImage(Stream stream)
+    {
+        using SKImage image = SKImage.FromEncodedData(stream);
+        return image.ToImage();
+    }
+}
